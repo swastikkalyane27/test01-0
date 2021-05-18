c5ba6e9d5348268aa9c907f1610187cac7615d22
Prasad Mule <prasadrmule47@gmail.com>
2021-05-18 19:57:35 +0530

    Added py

---
+++ str_formatting.py
@@ -0,0 +1,151 @@
+# name='Prasad'
+# city='Solapur'
+# dob_year=1993
+
+# print(f"Hello my name is {name} and living in {city} and my born year is {dob_year}")
+# print("Hello friends, my name is {} and basically from {} and my born year is {}".format(name,city,dob_year))
+# print("My name is %s" %(name))
+
+
+# list=[2,4,6,{1:'p'}]
+# for i in list:
+#     print(i*3)
+
+
+# temp=['Sharanya','Prasad','Pranav','Manajunath']
+# for i in temp:
+#     if i.startswith('P'):
+#         print(i)
+
+
+
+
+# lis1=[1,2,3,'abc','xyz']
+# lis2=[7,8,9,'abc']
+# # lis1.append(lis2)
+# # print(lis1)
+# lis1.extend(lis2)
+# print(lis1)
+
+
+# lis3=[1,2,3]
+# for i in range(len(lis3)):
+#     lis3.pop()
+# print(lis3)
+# 
+#     
+# ---------------------------------------------------------------------------Tuple--------------------------------------------
+
+# tup=(1,2,3,[10,2])
+# print(tup)
+# print(type(tup))
+# tup[0]=144
+# print(tup)
+
+
+# tup=(1,2,3,[10,2])
+# print(tup)
+# print(type(tup))
+# print(tup[3][0])
+# tup[3][0]=15
+
+# print(tup)
+
+
+# -----------------------------------------------Set-----------------
+# temp=set()
+# print(type(temp))
+
+# test={1,2,20,1,'abd','1111'}
+# test.update({'prasad':'one'})
+# print(test)
+
+
+# test.pop()
+# print(test)
+# test.
+# ()
+# print(test)
+
+
+# test2={'a','b','c'}
+# for i in test2:
+#     print(i)
+
+
+# --------------------------------------dict-------------------------
+
+
+
+# a={1:'a',2:'b'}
+# print(a)
+# for i,j in a.items():
+#     print(i,j) 
+
+
+# temp=[10,20,30]
+# a,b,c=temp
+# print(a)
+# print(b)
+
+
+# a="prasad"
+# b=""
+# print(a or b)
+
+
+print("Hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# test.add('prasad')
+# print(test)
+
+
+# test.update([7,8,500])
+# print(test)
+
+
+# test.remove(1)
+# print(test)
+
+
+# test.discard(200)
+# print(test)
+
+
+
+
+# test={1,2,20,1,'abd','1111'}
+# test1={55,22}
+# # tt=test+test1
+# tt=test.union(test1)
+# print(tt)
+
+
+
+
